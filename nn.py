a93cab73344f935f150c992004a025b9cf7e8f89
KASLIN Nando <nando.kaeslin@csem.ch>
2025-12-23 11:01:34 +0100

    formatting

--- nn.py
+++ nn.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 
 def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
     layers = []
-    for j in range(len(sizes)-1):
-        act = activation if j < len(sizes)-2 else output_activation
-        layers.append(nn.Linear(sizes[j], sizes[j+1]))
+    for j in range(len(sizes) - 1):
+        act = activation if j < len(sizes) - 2 else output_activation
+        layers.append(nn.Linear(sizes[j], sizes[j + 1]))
         layers.append(act())
     return nn.Sequential(*layers)
